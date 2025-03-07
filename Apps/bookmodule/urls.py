242022ecb535f589e68d9e638c7dee0557f1cedf
ishahdsu <shahd1791@hotmail.com>
2025-03-07 14:37:21 +0300

    lap5

--- Apps/bookmodule/urls.py
+++ Apps/bookmodule/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('list_books/', views.list_books, name= "books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links/', views.links, name='links'),
+    path('html5/text/formatting', views.formatting, name='formatting'),
+    path('html5/listing', views.listing, name='listing'),
+    path('html5/tables', views.tables, name='tables'),
+
+
 ]
 
  
--- Apps/bookmodule/views.py
+++ Apps/bookmodule/views.py
@@ -31,6 +31,20 @@ def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html', context)
 
 
+def links(request):
+    return render(request, 'bookmodule/links.html')
+
+
+def formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing(request):
+    return render(request, 'bookmodule/listing.html')
+
+
+def tables(request):
+    return render(request, 'bookmodule/tables.html')
+
  
 
 
